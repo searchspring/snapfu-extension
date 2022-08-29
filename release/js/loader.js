bfd479c5b6f125de71704fa405534e9b6db4fe32
searchspring-machine <machine@searchspring.com>
2022-08-29 16:51:31 +0000

    chore(release): 1.2.1

---
+++ release/js/loader.js
@@ -0,0 +1,1 @@
+(function(){"use strict";const e=document.getElementById("snapfu-script"),t=e?.innerText||"",n=e?.getAttribute("url"),c=document.createElement("script");c.id="searchspring-snapfu-script",c.src=n||"",c.innerHTML=t.trim(),(document.head||document.documentElement).appendChild(c)})();
