7edfa306989a019513993a363750b7df58b31b17
kevin <kevin@searchspring.com>
2022-10-24 12:52:15 -0600

    fix(inject): adding support for injecting and scraping iframes (for Bigcommerce)

--- src/entry/scraper.ts
+++ src/entry/scraper.ts
@@ -11,17 +11,25 @@ declare global {
 let data: LocalData = {};
 let timeElapsed = 0;
 
-const checker = setInterval(() => {
+setInterval(() => {
 	timeElapsed += SCRAPE_INTERVAL;
 	if (timeElapsed > 5000 && !data.version) {
-
 		// giving up scraping and nothing found
 		const event = new CustomEvent('snapfu-scrape', { detail: { timestamp: Date.now() } });
 		document.dispatchEvent(event);
 	}
-	
-	if (window?.searchspring) {
-		const searchspring = window.searchspring;
+
+	let browserGlobalSpace: Window | null | undefined = window;
+	if (!browserGlobalSpace?.searchspring) {
+		// if we don't find searchspring global on window try looking in iframes
+		const iframes = document.querySelectorAll('iframe');
+		browserGlobalSpace = Array.from(iframes)
+			.filter((iframe) => iframe.contentWindow?.searchspring)
+			?.pop()?.contentWindow;
+	}
+
+	if (browserGlobalSpace?.searchspring) {
+		const searchspring = browserGlobalSpace.searchspring;
 		const { controller, version, context } = searchspring;
 		const controllerIds = Object.keys(controller || {});
 		const controllers: ControllerInfo[] = controllerIds.map((controllerId) => {
@@ -32,7 +40,7 @@ const checker = setInterval(() => {
 				type,
 				store: {
 					loaded: store.loaded,
-					results: results ? new Array(results.length) : []
+					results: results ? new Array(results.length) : [],
 				},
 				collapsed: true,
 			};
@@ -43,24 +51,27 @@ const checker = setInterval(() => {
 			version,
 			context,
 		};
-		
+
 		const controllersAreTheSame = controllers.every((controller, index) => {
 			const controller2 = data.controllers && data.controllers[index];
 
-			return controller2 && controller.id === controller2.id && 
+			return (
+				controller2 &&
+				controller.id === controller2.id &&
 				controller.type === controller2.type &&
 				controller.store.loaded === controller2.store.loaded &&
-				controller.store.results.length === controller2.store.results.length;
+				controller.store.results.length === controller2.store.results.length
+			);
 		});
 
 		const versionsAreTheSame = data.version === newData.version;
-		
+
 		const resultsChanged = !controllersAreTheSame || !versionsAreTheSame;
 
 		// only send event when details have changed
 		if (newData.version != data.version || newData.controllers?.length != data.controllers?.length || resultsChanged) {
 			data = newData;
-			const payload = { timestamp: Date.now(), ...data }
+			const payload = { timestamp: Date.now(), ...data };
 			const event = new CustomEvent('snapfu-scrape', { detail: JSON.parse(JSON.stringify(payload)) });
 			document.dispatchEvent(event);
 		}
